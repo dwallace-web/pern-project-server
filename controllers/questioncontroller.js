881efde710cd60e2e1c0ff2b5b8a011d4803bdde
David Wallace <dwallacemarketing@gmail.com>
2020-11-10 19:17:52 -0500

    update question controller

--- controllers/questioncontroller.js
+++ controllers/questioncontroller.js
@@ -1,100 +1,100 @@
 const router = require('express').Router();
 const Question = require('../db').import('../models/question');
+
+
+
+// /* *********GET ALL QUESTIONS********* */
+
+// router.get("/", (req, res) => {
+//     Question.findAll()
+//     .then(questions => res.status(200).json(questions))
+//     .catch(err => res.status(500).json({ error: err}))
+// });
+
 const validateSession = require('../middleware/validate-session');
 
 
-    
-    /* *************QUESTION CREATE*************** */
-    
-    router.post('/create', validateSession, (req, res) => {
-        const questionEntry = {
-            title: req.body.question.title,
-            date: req.body.question.date,
-            entry: req.body.question.entry,
-            owner: req.user.id,
-        }
-        Question.create(questionEntry)
-            .then(question => res.status(200).json(question))
-            .catch(err => res.status(500).json({ error: err }))
-    });
-    
-   
 
 
+/* *************QUESTION CREATE*************** */
 
-    /* ***************EDIT QUESTIONS************** */
+router.post('/create', validateSession, (req, res) => {
+    const questionEntry = {
+        title: req.body.question.title,
+        category: req.body.question.category,
+        entry: req.body.question.entry,
+        owner: req.user.id,
+    }
+    Question.create(questionEntry)
+        .then(question => res.status(200).json(question))
+        .catch(err => res.status(500).json({ error: err }))
+});
 
-    router.put("/update/:entryId", validateSession, function (req, res) {
-        const updateQuestionEntry = {
-            title: req.body.question.title,
-            date: req.body.question.date,
-            entry: req.body.question.entry,
-        };
-    
-        const query = { where: { id: req.params.entryId, owner: req.user.id } };
-        
-        Question.update(updateQuestionEntry, query)
+/* ***************EDIT QUESTIONS************** */
+
+router.put("/update/:entryId", validateSession, function (req, res) {
+    const updateQuestionEntry = {
+        title: req.body.question.title,
+        date: req.body.question.date,
+        entry: req.body.question.entry,
+    };
+
+    const query = { where: { id: req.params.entryId, owner: req.user.id } };
+
+    Question.update(updateQuestionEntry, query)
         .then((questions) => res.status(200).json(questions))
-        .catch((err) => res.status(500).json({ error: err}));
-    });
+        .catch((err) => res.status(500).json({ error: err }));
+});
 
 
+/* ***************DELETE QUESTIONS************** */
 
-
-    /* ***************DELETE QUESTIONS************** */
-    
-    router.delete("/delete/:id", validateSession, function (req, res) {
-        const query = { where: { id: req.params.id, owner: req.user.id} };
-        Question.destroy(query)
-        .then(() => res.status(200).json({ message: "Question Entry Removed"}))
-        .catch((err) => res.status(500).json({ error: err}));
-    });
+router.delete("/delete/:id", validateSession, function (req, res) {
+    const query = { where: { id: req.params.id, owner: req.user.id } };
+    Question.destroy(query)
+        .then(() => res.status(200).json({ message: "Question Entry Removed" }))
+        .catch((err) => res.status(500).json({ error: err }));
+});
 
 /**EXTRA ENDPOINTS */
 
-    //  /* *********GET ALL QUESTIONS********* */
-    
-    // router.get("/", (req, res) => {
-    //     Question.findAll()
-    //     .then(questions => res.status(200).json(questions))
-    //     .catch(err => res.status(500).json({ error: err}))
-    // });
-    
-    
-    // /* **************GET QUESTIONS BY USER************** */
-    
-    // router.get("/mine", validateSession, (req, res) => {
-    //     let userid = req.user.id
-    //     Question.findAll({
-    //         where: { owner: userid }
-    //     })
-    //     .then(questions => res.status(200).json(questions))
-    //     .catch(err => res.status(500).json({ error: err}))
-    // });
-    
-    // /* ***************GET QUESTIONS BY TITLE************** */
-    
-    // router.get('/:title', function (req, res) {
-    //     let title = req.params.title;
-    
-    //     Question.findAll({
-    //         where: {title: title}
-    //     })
-    //     .then(questions => res.status(200).json(questions))
-    //     .catch(err => res.status(500).json({ error: err}))
-    // });
 
-    /* **************GET QUESTIONS BY CATEGORY*************** */
 
-    router.get('/:category', function (req, res) {
-        let category = req.params.category;
 
-        Question.findAll({
-            where: {category: category}
-        })
+// /* **************GET QUESTIONS BY USER************** */
+
+// router.get("/mine", validateSession, (req, res) => {
+//     let userid = req.user.id
+//     Question.findAll({
+//         where: { owner: userid }
+//     })
+//     .then(questions => res.status(200).json(questions))
+//     .catch(err => res.status(500).json({ error: err}))
+// });
+
+// /* ***************GET QUESTIONS BY TITLE************** */
+
+// router.get('/:title', function (req, res) {
+//     let title = req.params.title;
+
+//     Question.findAll({
+//         where: {title: title}
+//     })
+//     .then(questions => res.status(200).json(questions))
+//     .catch(err => res.status(500).json({ error: err}))
+// });
+
+/* **************GET QUESTIONS BY CATEGORY*************** */
+
+router.get('/:category', function (req, res) {
+    let category = req.params.category;
+
+    Question.findAll({
+        where: { category: category }
+    })
         .then(questions => res.status(200).json(questions))
-        .catch(err => resstatus(500).json({error: err}))
-    });
-    
-  
-    module.exports = router;
+        .catch(err => resstatus(500).json({ error: err }))
+});
+
+
+module.exports = router;
