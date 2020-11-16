2ac689a26f121083640bc9619b9873106db2b2cb
DeShawne <dmw606@gmail.com>
2020-11-15 21:39:14 -0500

    answer create tested in postman

--- models/question.js
+++ models/question.js
@@ -23,18 +23,6 @@ module.exports = (sequelize, DataTypes) => {
         // }
        
     });
-    // const User = sequelize.define('user', {
-    //     username: {
-    //         type: DataTypes.STRING,
-    //         allowNull: true,
-    //         unique: true
-    //     },
-    //     password: {
-    //         type: DataTypes.STRING,
-    //         allowNull: true
-    //     },
-    // });
-    // Question.belongsTo(User, {foreignKey: 'fk_userid', targetKey: 'id'});
     
     return Question
 };
